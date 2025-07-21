6ab5b29d959da82e9aee4d4ad438bf8ab53980a3
Simon Gezahagn <simongezahagn@gmail.com>
2025-07-21 22:19:02 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,20 +1,11 @@
-import './App.css';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import React from 'react';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-      <Footer />
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Simple Counter App</h1>
+      <Counter />
     </div>
   );
 }
