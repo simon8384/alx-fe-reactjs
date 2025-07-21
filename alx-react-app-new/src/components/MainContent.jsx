3bef6cd4c6acc981f19e098d6602e47a55c3b383
Simon Gezahagn <simongezahagn@gmail.com>
2025-07-21 21:57:43 +0300

    First stage

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f8ff', padding: '20px', textAlign: 'center' }}>
+      <p style={{ fontSize: '1.2rem', color: '#333' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
